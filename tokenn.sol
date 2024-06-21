e60272c5997eab2938732097846f7f4336d2773c
Akash kumar (sky) <113254843+theakash07@users.noreply.github.com>
2024-06-21 22:13:40 +0530

    ETHBeginner.sol

--- tokenn.sol
+++ tokenn.sol
@@ -1,34 +1,25 @@
-// SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
+contract MyToken {
 
-contract nbtoken {
+    // Public variables to store the details about the coin
+    string public tokken_name = "META_Mytokken";
+    string public tokken_abbrv = "MTA";
+    uint public totalSupply= 0;
 
-    // public variables here
-    string public tokenName = "NOBUDDY";
-    string public tokenAbbrv = "NB";
-    uint public totalSupply = 0;
+    // Mapping to store balances of addresses
+    mapping(address => uint ) public balances;
 
-    // mapping variable here
-
-    mapping (address => uint) public balances;
- 
-    // mint function
-
-    function mint(address _address, uint _amount) public {
-
-        totalSupply += _amount;
-        balances[_address] += _amount;
+    // Mint function to increase total supply and balance of sender address
+    function mint(address _address, uint _value) public {
+        totalSupply += _value;
+        balances[_address] += _value;
     }
 
-    // burn function
-
-    function burn(address _address, uint _amount) public{
-
-        if (balances[_address] >= _amount){
-            totalSupply -= _amount;
-            balances[_address] -= _amount;
-        }
-        
-    }
-
+    // Burn function to decrease total supply and balance of sender address
+    function burn(address _address, uint _value) public {
+       if (balances[_address] >= _value) {
+        totalSupply -= _value;
+        balances[_address] -= _value;
+       }
+}
 }
